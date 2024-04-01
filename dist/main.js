f731bf3a6dbfd9e7a48a4694816b1aa332952228
Vovegog <daniel.viggen@hotmail.com>
2024-04-01 12:04:10 +0200

    ..

--- dist/main.js
+++ dist/main.js
@@ -1,17 +1,8 @@
+"use strict";
 // Import any necessary modules or libraries here
-
-import { createRoot } from 'react-dom/client';
-
+Object.defineProperty(exports, "__esModule", { value: true });
 // Define scripting functions here
-
 document.body.innerHTML = '<div id="root"></div>';
-
 const root = document.getElementById('root');
-
-
 // Define React components here
-
-
-
 // Export functions or variables here
-
---
+++ dist/server.js
@@ -0,0 +1,23 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = __importDefault(require("express"));
+const path_1 = __importDefault(require("path"));
+const app = (0, express_1.default)();
+const port = 80;
+app.use(express_1.default.json());
+app.use(express_1.default.static(path_1.default.join(__dirname)));
+app.use(function (req, res, next) {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+    res.setHeader('Access-Control-Allow-Credentials', 'true');
+    next();
+});
+// POST method's go here
+// GET method's go here
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
