da8774b3ee524ee6fdeb8fc88b1642a5fbb978aa
Daniel <126813872+Vovegog@users.noreply.github.com>
2024-04-01 11:26:16 +0200

    Still setting up

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,14 @@
 // Import any necessary modules or libraries here
 
+import React from 'react';
+
 // Define scripting functions here
 
+
+
+// Define React components here
+
+
+
 // Export functions or variables here
+
